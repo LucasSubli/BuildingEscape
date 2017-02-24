dbfa534ec090cf33f9843529017717a7c58bd515
Lucas.Subli <lucas.subli@gmail.com>
2017-02-24 20:38:17 -0300

    Making the Trigger Volume open the doors

---
+++ README.md
@@ -0,0 +1,16 @@
+## BuildingEscape
+
+My first attempt at using the unreal Engine 4 to make an escape game
+
+## Installation
+
+To run the code you will need to include the UE4 Starter content manually into the appropriate folder beforehand.
+
+## Contributors
+
+Let people know how they can dive into the project, include important links to things like issue trackers, irc, twitter accounts if applicable.
+
+## License
+
+WTFPL (Do What the Fuck You Want To Public License).
+https://en.wikipedia.org/wiki/WTFPL
---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+}
+
+void UOpenDoor::OpenDoor() {
+	AActor* Owner = GetOwner();
+
+	FRotator NewRotation = FRotator(0.f, 120.f, 0.f);
+
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Poll the Trigger every frame
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens)) {
+		OpenDoor();
+	}
+}
+
